15637724cca1550d2bf047fb9731fdf6be02a5cc
Davi Bernardo <davi.santos@extreme.digital>
2021-12-30 09:39:15 -0300

    Por que usar TypeScript?

---
+++ 9-typescript/9-1-por-que-usar-TypeScript/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const me = {
+    name: 'Davi',
+    age: 37
+};
+exports.default = me;
---
+++ 9-typescript/9-1-por-que-usar-TypeScript/index.ts
@@ -0,0 +1,6 @@
+const me  = {
+    name: 'Davi',
+    age: 37
+}
+
+export default me
