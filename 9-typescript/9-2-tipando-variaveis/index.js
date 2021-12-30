fd4453b7c0cc3e7d7dd1d4c48d911902575ea1f1
Davi Bernardo <davi.santos@extreme.digital>
2021-12-30 10:44:54 -0300

    Tipando variáveis

---
+++ 9-typescript/9-2-tipando-variaveis/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const personName = 'Davi';
+const personAge = 22;
+const personAlive = true;
+const personContacts = [
+    0,
+    'dnssites@gmail.com'
+];
---
+++ 9-typescript/9-2-tipando-variaveis/index.ts
@@ -0,0 +1,8 @@
+const personName:string = 'Davi'
+const personAge:number = 22
+const personAlive:boolean = true
+
+const personContacts:(string|number)[]=[
+    0,
+    'dnssites@gmail.com'
+]
